44ff183a5ef793056b38370179c82f71e327a83c
Dkhodos <dkhodos94@gmail.com>
2021-06-26 21:22:28 +0300

    github pages doesn't support normal routing, move to hash routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
 import App from './App'
-import {BrowserRouter as Router} from "react-router-dom";
+import {BrowserRouter as Router, HashRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
         <Router basename={"/omdb-search"}>
-            <App/>
+            <HashRouter>
+                <App/>
+            </HashRouter>
         </Router>
     </React.StrictMode>,
     document.getElementById('root')
