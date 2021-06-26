3c1046e22a9d55ad1b779648e3f5899eb0e82067
Dkhodos <dkhodos94@gmail.com>
2021-06-26 21:10:43 +0300

    fix https / http conflict

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import {BrowserRouter as Router} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
+        <Router basename={"/omdb-search"}>
             <App/>
         </Router>
     </React.StrictMode>,
